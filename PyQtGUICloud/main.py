3f631d9119292fbbbc6c5cff148c4fae7ffe7a18
fanqiangdatui <fanqiangdatui@gmail.com>
2021-12-28 18:44:41 +0800

    QT GUI

---
+++ PyQtGUICloud/main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQtCloudGUI import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+
+
+class MainWindow(QMainWindow, Ui_Form):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.textEdit.setText('fgjhsjsf')
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
